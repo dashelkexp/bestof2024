84aaf03f10f1c4e05188eb715c81a7bc5b123900
dashelkexp <dashel@kexp.org>
2024-10-17 14:31:20 -0700

    JS

---
+++ script.js
@@ -0,0 +1,26 @@
+// Example data for albums
+const albums = [
+    {
+        title: "Album 1",
+        img: "https://via.placeholder.com/150",
+        review: "This is a 200-word review of Album 1. Lorem ipsum...",
+        quote: "Best album of the year.",
+        links: {
+            bandcamp: "#",
+            spotify: "#",
+            apple: "#"
+        }
+    },
+    {
+        title: "Album 2",
+        img: "https://via.placeholder.com/150",
+        review: "This is a 200-word review of Album 2. Lorem ipsum...",
+        quote: "A masterpiece in modern music.",
+        links: {
+            bandcamp: "#",
+            spotify: "#",
+            apple: "#"
+        }
+    },
+    // Add more album objects here, up to 100 albums
+];
