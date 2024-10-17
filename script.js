1de9fd9eded4600a72ce827add86de678a63ab6a
dashelkexp <dashel@kexp.org>
2024-10-17 14:52:37 -0700

    add djs

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const albums = [
         img: "https://via.placeholder.com/150",
         review: "This is a 200-word review of Album 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
         quote: "Best album of the year.",
+        mostPlayed: ["DJ Alpha", "DJ Beta", "DJ Gamma"],  // Add up to 3 DJs
         links: {
             bandcamp: "#",
             spotify: "#",
@@ -16,6 +17,7 @@ const albums = [
         img: "https://via.placeholder.com/150",
         review: "This is a 200-word review of Album 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
         quote: "A masterpiece in modern music.",
+        mostPlayed: ["DJ Zeta"],  // Add DJ(s) here
         links: {
             bandcamp: "#",
             spotify: "#",
@@ -28,6 +30,7 @@ const albums = [
         img: "https://via.placeholder.com/150",
         review: "This is a 200-word review of Album 100. Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
         quote: "Incredible soundscapes and storytelling.",
+        mostPlayed: ["DJ Delta", "DJ Omega"],  // Add DJ(s) here
         links: {
             bandcamp: "#",
             spotify: "#",
@@ -40,6 +43,19 @@ const albums = [
 function addAlbum(album) {
     const albumList = document.getElementById('album-list');
     
+    // Build the "Most Played" section (handles 0, 1, 2, or 3 DJs)
+    let mostPlayedSection = '';
+    if (album.mostPlayed && album.mostPlayed.length > 0) {
+        mostPlayedSection = `
+            <div class="most-played mt-3">
+                <h6>Most Played by:</h6>
+                <ul class="list-unstyled">
+                    ${album.mostPlayed.map(dj => `<li>${dj}</li>`).join('')}
+                </ul>
+            </div>
+        `;
+    }
+
     // Create a new card for the album
     const albumCard = `
         <div class="col-md-4">
@@ -51,6 +67,7 @@ function addAlbum(album) {
                     <blockquote class="blockquote mb-0">
                         <p>"${album.quote}"</p>
                     </blockquote>
+                    ${mostPlayedSection}  <!-- Inserted Most Played section -->
                     <div class="mt-3">
                         <a href="${album.links.bandcamp}" class="btn btn-sm btn-outline-primary">Listen on Bandcamp</a>
                         <a href="${album.links.spotify}" class="btn btn-sm btn-outline-secondary">Listen on Spotify</a>
